af796a04f10fc02a76623a0add6d0f2aa7346143
YouGuavas <patrickyambrick@gmail.com>
2018-05-23 12:25:23 -0400

    Change how to add commands. Help command now also reads from the command object.

--- bot.js
+++ bot.js
@@ -5,10 +5,7 @@ const cors = require('cors');
 const cheerio = require('cheerio');
 const dotenv = require('dotenv').config();
 
-const Stats = require('./commands/stats');
-const Login = require('./commands/login');
-const Boss = require('./commands/bosses');
-const Skills = require('./commands/skills');
+const Commands = require('./commands/commands');
 
 const client = new Discord.Client();
 const prefix = process.env.PREFIX;
@@ -17,41 +14,8 @@ const prefix = process.env.PREFIX;
 const app = express();
 app.use(cors());
 
-let rg_sess = {
-	session: ''
-}
-const help = {
-	ping: {
-		message: 'Returns a pong. Helps to check if bot is listening.',
-		usage: `${prefix}pong`
-	},
-	images: {
-		message: 'Returns a google image search matching <query>.',
-		usage: `${prefix}images <query>`
-	},
-	purge: {
-		message: 'Cleans channel of past [number] messages. <number> defaults to 100.',
-		usage: 	`${prefix}purge [number]`
-	},
-	stats: {
-		message: 'Returns a basic profile of <characterName>.',
-		usage: `${prefix}stats <characterName>`
-	},
-	bosses: {
-		message: 'Returns a list of bosses currently alive, and their percentages.',
-		usage: `${prefix}bosses`
-	},
-	login: {
-		message: 'Updates session id of checker/former.',
-		usage: `${prefix}login`
-	},
-	train: {
-		message: 'Trains dc skills{circ, haste, stone skin}, on <characterID>, <level> number of times.',
-		usage: `${prefix}train <loginUsername> <loginPassword> <characterID> <level>`
-	}
-}
 client.on("ready", () => {//
-	Login.login(process.env.OW_USERNAME, process.env.OW_PASSWORD, process.env.BASE, {session:rg_sess});
+	Commands.commands.login.fxn();
 	console.log("I am ready!");
 })
 client.on("message", (message) => {
@@ -60,78 +24,10 @@ client.on("message", (message) => {
 
 	const command = message.content.slice(1, message.length).trim().split(/ +/g)[0];
 	const args = message.content.slice(1, message.length).trim().split(/ +/g).slice(1);
-
-	if (command === "ping") {
-		message.channel.send("pong!");
-	}
-	if (command === 'images') {
-		if (args.length === 0) {
-			message.delete().catch(o_O => {});
-			return message.reply("Please provide a query to search for");
-		}
-		const searchString = args.join('+');
-		const url = 'http://www.google.com/search?oq='+searchString+'&q='+searchString+'&tbm=isch';
-		message.delete().catch(o_O=>{});
-		return message.channel.send(url);	 
-	}
-	if (command === 'purge') {
-		const deleteCount = parseInt(args[0], 10) || 100;
-    
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    message.channel.fetchMessages({count: deleteCount})
-    	.then(fetched => {
-    			message.channel.bulkDelete(fetched)
-						.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-					return message.reply("Successfully purged the channel.");
-    	});
-	}
-	//
-	if (command === 'stats') {
-		if (args.length === 0) {
-			return message.reply('Please provide a character to check stats for');
-		}
-		const searchString = args[0];
-		return Stats.stats(searchString, process.env.BASE, {message: message});
-	}
-	//
-	if (command === 'login') {
-		return Login.login(process.env.OW_USERNAME, process.env.OW_PASSWORD, process.env.BASE, {session: rg_sess, message: message});
-	}
-	if (command === 'logout') {
-		return Login.logout(process.env.BASE, {message: message});
-	}
-	//
-	if (command === 'bosses') {
-		return Boss.bossStats(process.env.BASE, {session: rg_sess, message: message, serverid: process.env.SERVERID, checker: process.env.CHECKER});
-	}
-	if (command === 'boss-stats') {
-		return Boss.statsBoss(process.env.BASE, args[0], {session: rg_sess, message: message, serverid: process.env.SERVERID, checker: process.env.CHECKER});
-	}
-	//
-	if (command === 'train') {
-		message.delete().catch(o_O => {});
-		return Skills.dcTrain(process.env.BASE, args[0], args[1], args[2], args[3], {login: Login.login, message: message, serverid: process.env.SERVERID});
-	}
-
-	if (command === 'help') {
-		let msg = '';
-		args.length === 0 ? (
-			msg = '**Commands:**',
-			Object.keys(help).map((item, index) => {
-				index === Object.keys(help).length - 1 ? msg += ` ${item}` : msg += ` ${item},`
-			}),
-			message.reply(msg)
-			) : (
-			msg = `\n**${args[0].toLowerCase()}:**`,
-			Object.keys(help[args[0].toLowerCase()]).map(item => {
-				msg += `\n**${item}**:\n${help[args[0].toLowerCase()][item]}`
-			}),
-			message.reply(msg)
-			)
-			return;
-	}
+	
+	Object.keys(Commands.commands).indexOf(command) !== -1 ? (Object.keys(Commands.commands).map(item => {
+		item.toLowerCase() === command.toLowerCase() ? Commands.commands[item].fxn(message, args) : '';
+	})) : message.reply(`I'm sorry, I don't recognize that command.`);
 });
 
 client.login(process.env.AUTH);
---
+++ commands/commands.js
@@ -0,0 +1,138 @@
+const Stats = require('./stats');
+const Login = require('./login');
+const Boss = require('./bosses');
+const Skills = require('./skills');
+const World = require('./world');
+
+const dotenv = require('dotenv').config();
+const prefix = process.env.PREFIX;
+let rg_sess = {
+	session: ''
+}
+
+exports.commands = {
+	'ping': {
+		fxn: (message) => {message.channel.send('pong!')},
+		message: 'Returns a pong. Helps to check if bot is listening.',
+		usage: `${prefix}pong`
+	},
+	'images': {
+		fxn: (message, args) => {
+			if (args.length === 0) {
+				message.delete().catch(o_O => {});
+				return message.reply("Please provide a query to search for");
+			}
+			const searchString = args.join('+');
+			const url = 'http://www.google.com/search?oq='+searchString+'&q='+searchString+'&tbm=isch';
+			message.delete().catch(o_O=>{});
+			return message.channel.send(url);	 
+		},
+		message: 'Returns a google image search matching <query>.',
+		usage: `${prefix}images <query>`
+
+	},
+	'purge': {
+		fxn: (message,args) => {
+			const deleteCount = parseInt(args[0], 10) || 100;
+	    
+	    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+	      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+	    
+	    message.channel.fetchMessages({count: deleteCount})
+	    	.then(fetched => {
+	    			message.channel.bulkDelete(fetched)
+							.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+						return message.reply("Successfully purged the channel.");
+	    	});
+	    },
+	  message: 'Cleans channel of past [number] messages. <number> defaults to 100.',
+		usage: 	`${prefix}purge [number]`
+
+	},
+
+
+	'stats': {
+		fxn: (message, args) => {
+			if (args.length === 0) {
+				return message.reply('Please provide a character to check stats for');
+			}
+			const searchString = args[0];
+			return Stats.stats(searchString, process.env.BASE, {message: message});
+		},
+		message: 'Returns a basic profile of <characterName>.',
+		usage: `${prefix}stats <characterName>`
+	},
+
+
+	'login': {
+		fxn: (message, args) => {
+			return Login.login(process.env.OW_USERNAME, process.env.OW_PASSWORD, process.env.BASE, {session: rg_sess, message: message});
+		},
+		message: 'Updates session id of checker/former.',
+		usage: `${prefix}login`
+	},
+	'logout': {
+		fxn: (message, args) => {
+			return Login.logout(process.env.BASE, {message: message});
+		}
+	},
+
+
+	'bosses': {
+		fxn: (message, args) => {
+			return Boss.bossStats(process.env.BASE, {session: rg_sess, message: message, serverid: process.env.SERVERID, checker: process.env.CHECKER});
+		},
+		message: 'Returns a list of bosses currently alive, and their percentages.',
+		usage: `${prefix}bosses`
+	},
+	'boss-stats': {
+		fxn: (message, args) => {
+			return Boss.statsBoss(process.env.BASE, args[0], {session: rg_sess, message: message, serverid: process.env.SERVERID, checker: process.env.CHECKER});
+		},
+		message: 'Returns damage and drop status for <bossName>. Attempts to match the beginning of the boss\'s name.',
+		usage: `${prefix}boss-stats <bossName>`
+	},
+
+
+	'train': {
+		fxn: (message, args) => {
+			message.delete().catch(o_O => {});
+			return Skills.dcTrain(process.env.BASE, args[0], args[1], args[2], args[3], {login: Login.login, message: message, serverid: process.env.SERVERID});
+		},
+		message: 'Trains dc skills{circ, haste, stone skin}, on <characterID>, <level> number of times.',
+		usage: `${prefix}train <loginUsername> <loginPassword> <characterID> <level>`
+	},
+
+
+	'attack': {
+		fxn: (message, args) => {
+			message.delete().catch(o_O => {});
+			return World.attackRoom(process.env.BASE, args[0], args[1], args[2], args[3], {login: Login.login, message: message, serverid: process.env.SERVERID});
+		},
+		message: 'Attacks all mobs in current room <number> of times.',
+		usage: `${prefix}attack <loginUsername> <loginPassword> <characterID> <number>`
+	},
+
+	'help': {
+		fxn: (message, args) => {
+			let msg = '';
+			args.length === 0 ? (
+				msg = '**Commands:**',
+				Object.keys(this.commands).map((item, index) => {
+					index === Object.keys(this.commands).length - 1 ? msg += ` ${item}` : msg += ` ${item},`
+				}),
+				message.reply(msg)
+				) : (
+				msg = `\n**${args[0].toLowerCase()}:**`,
+				Object.keys(this.commands[args[0].toLowerCase()]).map(item => {
+					item !== 'fxn' ? msg += `\n**${item}**:\n${this.commands[args[0].toLowerCase()][item]}` : null;
+				}),
+				message.reply(msg)
+				)
+				return;
+		},
+		message: 'Returns a list of all available commands, or how to use a specific command.',
+		usage: `${prefix}help [command]`
+
+	}
+}
