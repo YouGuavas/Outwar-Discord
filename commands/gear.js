3de02edc7b2d63a1b4d81b56fd0d5d0dd60fcbf2
YouGuavas <patrickyambrick@gmail.com>
2018-06-05 16:09:07 -0400

    Update gear caching

--- commands/gear.js
+++ commands/gear.js
@@ -27,27 +27,68 @@ exports.myGear = (url, id, stuff, cb = () => {}) => {
 }
 
 exports.skillGear = (url, id, stuff, cb = () => {}) => {
-	axios.get(`${url}backpack.php?suid=${id}&rg_sess_id=${stuff.session.session}&serverid=${stuff.serverid}&orb=1`)
-		.then(res => {
-			const results = res.data.split('img');
-			let items = {};
-			results.slice(1).map(item => {
-				const itemId = item.split(`itempopup(event,'`)[1].split(`')"`)[0];
-				stuff.message.reply('hey');
+	let items = {
+		orbs: {},
+		pants: {}
+	};
+	const slots = {
+		'8': 'orbs',
+		'9': 'pants'
+	}
+	Object.keys(slots).map(async slot => {
+		await (
+		axios.get(`${url}backpack.php?suid=${id}&rg_sess_id=${stuff.session.session}&serverid=${stuff.serverid}${slots[slot] === 'orbs' ? '&orb=1' : ''}`)
+			.then(res => {
+				let itemIds = [];
+				const results = res.data.split('img');
+				results.slice(1).map(item => {
+					let itemId;
+						item.indexOf(`'${slot}');`) !== -1 ? (itemId = item.split(`itempopup(event,'`)[1],
+						(itemId && itemId.indexOf(`')"`) !== -1) ? (
+						itemId = itemId.split(`')"`)[0],
+						itemId.length > 0 ? itemIds.push(itemId) : ''
+						) : null ) : '';
+					//console.log(itemId);
+					//stuff.message.reply('hey');
+				})
+				checkStats(url, id, slots[slot], {message: stuff.message, serverid: stuff.serverid, session: stuff.session, ids: itemIds}, (skillItems, itemId) => {
+					items[slots[slot]][itemId] = skillItems;
+					console.log(items);
+				})
 			})
-			cb(items);
-			return items;
+			.catch(o_O => {
+				console.log(o_O);
+				return stuff.message.reply('Error. Please check logs.');
+			})
+		)
+	})
+		console.log(items);
+		cb(items);
+		return items;
+}
+
+
+const checkStats = (url, id, slot, stuff, cb = () => {}) => {
+	let skillItems = {};
+	stuff.ids.map(async item => {
+		await (axios.get(`${url}itemlink.php?suid=${id}&rg_sess_id=${stuff.session.session}&id=${item}&serverid=${stuff.serverid}`)
+			.then(res => {
+			//console.log(res.data);
+			const skills = res.data.split('color:#00FF00').slice(1);
+			let iskills = {};
+			skills.map(item => {
+				const skill = item.split('>')[1].split('<')[0];
+				iskills[skill.split(' ')[1]] = skill.split('+')[1].split(' ')[0];
+			})
+			skillItems = iskills;
+			cb(skillItems, item);
 		})
 		.catch(o_O => {
 			console.log(o_O);
 			return stuff.message.reply('Error. Please check logs.');
 		})
-}
-
-
-const checkStats = (item, id, stuff) => {
-	axios.get(`${url}itemlink.php?suid=${id}&rg_sess_id=${stuff.session.session}&serverid=${stuff.serverid}`)
-
+		)
+	})
 }
 
 
